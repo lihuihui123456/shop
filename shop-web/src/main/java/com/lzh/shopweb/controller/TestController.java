71a05661ba5f06406733465a5816993d0d8e41bc
ame <1833607032@163.com>
2019-12-27 17:21:45 +0800

    配置springmvc和freemaker

--- shop-web/src/main/java/com/lzh/shopweb/controller/TestController.java
+++ shop-web/src/main/java/com/lzh/shopweb/controller/TestController.java
@@ -1,13 +1,18 @@
 package com.lzh.shopweb.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.lzh.shopservice.GgcStockService;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -17,20 +22,54 @@ import java.util.Map;
  * @Date: 2019/12/23 10:51
  * @Description:
  */
-@RestController
+@Controller
 @RequestMapping("/user")
 public class TestController {
     @Autowired
     private GgcStockService ggcStockService;
 
     @RequestMapping("test")
-    @ResponseBody
-    public ModelAndView selectUUser(Model model){
+    public String selectUUser(Model model){
         int data = ggcStockService.findData();
-        Map<String, Object> data1 = new HashMap<>(2);
-        data1.put("name", "springboot Freemarker");
-        data1.put("now", new Date());
-        //如何配置试图解析器
-        return new ModelAndView("index", data1);
+        model.addAttribute("name", "springboot Freemarker");
+        return "index";
+    }
+    @RequestMapping("test3")
+    @ResponseBody  //此标签表示返回json等数据格式 不需要解析视图解析器
+    public String selectUUser3(Model model){
+        int data = ggcStockService.findData();
+        model.addAttribute("name", "springboot Freemarker");
+        return "index";
+    }
+    @RequestMapping("test1")
+    @ResponseBody  //此标签表示返回json等数据格式 不需要解析视图解析器
+    public void selectUUser1(HttpServletResponse response) throws Exception{
+        JSONObject json = new JSONObject();
+        String code = "0000";
+        String msg = "ok";
+        String totalAmount = "15555";
+        Map<String ,Object> map = new HashMap<>();
+        try {
+
+        } catch (Exception e) {
+            code = "0001";
+            msg = e.getMessage();
+        }
+        json.put("code", code);
+        json.put("msg", msg);
+        double total = Double.parseDouble(totalAmount);
+        totalAmount = String.format("%.2f", total);
+        json.put("totalAmount", totalAmount);
+        json.put("totalAmount_td", String.format("%.2f",map.get("totalAmount_td")));
+        json.put("totalNum_td", map.get("totalNum_td"));
+        this.response(response, json.toString());
+    }
+
+    protected void response(HttpServletResponse response, String responseMsg) throws Exception {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.write(responseMsg);
+        out.flush();
+        out.close();
     }
 }
